6aff3a653f1cd6f5f82c4af892fa68caf66576fb
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-05-03 13:01:37 -0700

    finishing up employees

--- sms/lib/sms/human_resources.ex
+++ sms/lib/sms/human_resources.ex
@@ -1,5 +1,30 @@
 defmodule Sms.HumanResources do
-  def list_employees do
 
+  alias Sms.HumanResources.Employee
+  # @url "http://localhost:4000/api/employees"
+
+  def list_employees do
+    with {:ok, response} = HTTPoison.get("http://localhost:4000/api/employees"),
+      {:ok, values} = Jason.decode(response.body) do
+        keys_to_atoms(values["data"])
+        |> Enum.map(&(struct(Employee, &1)))
+      end
   end
+
+  def get_employee(id) do
+    with {:ok, response} = HTTPoison.get("http://localhost:4000/api/employees" <> "/#{id}"),
+      {:ok, values} = Jason.decode(response.body) do
+        IO.inspect(values)
+      end
+  end
+
+  defp keys_to_atoms(json_list) do
+    json_list
+    |> Enum.map(fn row ->
+      row
+      |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
+    |> Enum.into(%{})
+    end)
+  end
+
 end
